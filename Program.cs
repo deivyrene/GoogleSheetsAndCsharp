46e4aa758785660f97e3cf8aeda5899ace09c407
Deivy Hernandez <deivyhernandezperez@gmail.com>
2021-09-30 21:04:47 -0300

    Create connection with csharp and google sheet

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+
+namespace GoogleSheetsAndCsharp
+{
+  class Program
+  {
+    static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+    static readonly string ApplicationName = "Aplication Name";
+    static readonly string SpreadsheetId = "spreadsheet ID";
+    static readonly string sheet = "spreadsheet name";
+    static SheetsService service;
+
+    static void Main(string[] args)
+    {
+      GoogleCredential credential;
+      using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+      {
+        credential = GoogleCredential.FromStream(stream)
+            .CreateScoped(Scopes);
+      }
+
+      // Create Google Sheets API service.
+      service = new SheetsService(new BaseClientService.Initializer()
+      {
+        HttpClientInitializer = credential,
+        ApplicationName = ApplicationName,
+      });
+
+      ReadEntries();
+    }
+
+    static void ReadEntries()
+    {
+      var range = $"{sheet}!A:N";
+      SpreadsheetsResource.ValuesResource.GetRequest request =
+              service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+      var response = request.Execute();
+      var values = response.Values;
+      if (values != null && values.Count > 0)
+      {
+        foreach (var row in values)
+        {
+          // Print columns A to N, which correspond to indices 0 and 4.
+          Console.WriteLine("{0} | {1} | {2} | {3} | {4} | {5}", row[0], row[1], row[2], row[3], row[4], row[5]);
+        }
+      }
+      else
+      {
+        Console.WriteLine("No data found.");
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+# GoogleSheetsAndCsharp
+
+## Requirement
+
+- Have installed the [dotnet](https://dotnet.microsoft.com/download) version at least 5.0.401
+- Have credentials from Google Api platform:
+  - Go to the Google APIs Console.
+  - Create a new project.
+  - Click Enable API. Search for and enable the Google Drive API and the Google Sheets API.
+  - Create credentials for a Web Server to access Application Data.
+  - Name the service account and grant it a Project Role of Editor.
+  - Download the JSON file and paste in the root of the project.
+
+## Configuration
+
+- Indicate the name of the project
+
+```c#
+  static readonly string ApplicationName = "Aplication Name";
+```
+
+- Indicate the ID of the google spreadsheet spreadsheet
+
+```c#
+  static readonly string SpreadsheetId = "spreadsheet ID";
+```
+
+- Indicate the name of the spreadsheet (The name that is reflected in the bottom tab of the sheet)
+
+```c#
+  static readonly string sheet = "spreadsheet name";
+```
+
+- Indicate range of columns
+
+```c#
+  var range = $"{sheet}!A:N";
+```
+
+## Execute
+
+Install GoogleApisSheet
+
+```c#
+dotnet add package Google.Apis.Sheets.v4
+```
+
+From the root of the project, execute `dotnet run`, must be printed in console, the items consulted
